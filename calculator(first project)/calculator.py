0c73d4f8fbb815da350493078051977fdd10987a
Herghelegiu Cristian-Gabriel <herghelegiucristiangabriel2@gmail.com>
2026-05-14 00:02:11 +0300

    new method to calculate an expression you can now type full expressions to be evaluated also changed the result print method

--- calculator(first project)/calculator.py
+++ calculator(first project)/calculator.py
@@ -1,44 +1,90 @@
-from operations import add, subtract, multiply, divide, clear, initialRead
+from operations import add, subtract, multiply, divide, clear, initialRead, evaluateExpression
 
 class Calculator:
     # the starting menu message
     INITIAL_MESSAGE = """The calculator has been started, if you want to stop it just type stop | quit | q
-Options:
+You can use 'mode' -> select the operation mode if you want to change the operation mode(more infos when you type 'mode')
+Insert your starting mode: """
+    NORMAL_MESSAGE = """Options:
 'add' -> addition | 'substract' -> substraction | 'multiply' -> multiplication | 'divide' -> division
 'clear' -> clears the result"""
+    COMPLETE_MESSAGE = """Just type in the expression that you want to evaluate(for example: 1+2+3*(4/5))"""
+    MODES_MESSAGE = """Modes: 'Normal' -> you insert the desired operation and operand one by one
+'Complete' -> you can insert a whole expressions
+Your Mode: """
+    MODES_ERROR = """ERROR: You entered an invalid mode!"""
+    MODES = ["Normal", "Complete"]
+    OPERATORS = {"+":1, "-":1, "*":2, "/":2} # key = operator, value = priority
 
     def __init__(self) -> None:
         self.result = 0.0
 
-    def selector(self, selection) -> None:
-        match selection:
-            case "add":
-                add(self)
-            case "substract":
-                subtract(self)
-            case "multiply":
-                multiply(self)
-            case "divide":
-                divide(self)
-            case "clear":
-                clear(self)
-            case _:
-                print("Invalid option!")
-
-    def print(self) -> None:
-        if(self.result.is_integer()):
-            print(int(self.result))
+        initial_mode = input(Calculator.INITIAL_MESSAGE)
+        if initial_mode in Calculator.MODES:
+            self.mode = initial_mode
         else:
-            print(self.result)
+            while initial_mode not in Calculator.MODES:
+                print(Calculator.MODES_ERROR)
+                initial_mode = input("Your starting mode: ")
+            self.mode = initial_mode
+
 
     def start(self) -> None:
-        print(self.INITIAL_MESSAGE)
-        initialRead(self)
+        if self.mode == "Normal":
+            print(Calculator.NORMAL_MESSAGE)
+            initialRead(self)
+        else:
+            print(Calculator.COMPLETE_MESSAGE)
 
         # infinite loop until you quit
         while True:
-            selection = input("Your selection: ").lower()
+            selection = input("Your input: ").lower()
             if (selection in ("stop", "quit", "q")):
                 break
             else:
                 self.selector(selection)
+
+
+    def selector(self, selection) -> None:
+        match selection:
+            case "add" if self.mode == "Normal":
+                add(self)
+            case "substract" if self.mode == "Normal":
+                subtract(self)
+            case "multiply" if self.mode == "Normal":
+                multiply(self)
+            case "divide" if self.mode == "Normal":
+                divide(self)
+            case "clear":
+                clear(self)
+                if self.mode == "Normal":
+                    initialRead(self)
+            case "mode":
+                self.selectMode()
+            case _:
+                if Calculator.isExpression(selection) and self.mode == "Complete":
+                    self.result = evaluateExpression(selection)
+                    print(self)
+                    return
+                print("Invalid option!")
+
+
+    def selectMode(self):
+        mode = input(Calculator.MODES_MESSAGE)
+        if mode in Calculator.MODES and mode != self.mode:
+            self.mode = mode
+            if mode == "Normal":
+                print(Calculator.NORMAL_MESSAGE)
+                initialRead(self)
+
+
+    def __str__(self):
+        return f"Result: {self.result:g}"
+
+
+    @staticmethod
+    def isExpression(selection) -> bool:
+        for char in selection:
+            if char in Calculator.OPERATORS:
+                return True
+        return False
--- calculator(first project)/operations.py
+++ calculator(first project)/operations.py
@@ -1,27 +1,109 @@
+import math
+
 GENERAL_MESSAGE = "Operation: {operation} | Your number: "
+CONSTANTS = {
+    "pi": math.pi,
+    "e": math.e
+}
+
+def readFloat(message= "Your number: ") -> float:
+    inputVal = input(message).lower()
+    if inputVal in CONSTANTS:
+        inputVal = CONSTANTS[inputVal]
+    return float(inputVal)
+
 
 def initialRead(calculator) -> None:
     calculator.result = readFloat("Insert your starting number: ")
 
-def readFloat(message= "Your number: ") -> float:
-        return float(input(message))
 
 def add(calculator) -> None:
     calculator.result += readFloat(GENERAL_MESSAGE.format(operation = "addition"))
-    calculator.print()
+    print(calculator)
+
 
 def subtract(calculator) -> None:
     calculator.result -= readFloat(GENERAL_MESSAGE.format(operation = "subtraction"))
-    calculator.print()
+    print(calculator)
+
 
 def multiply(calculator) -> None:
     calculator.result *= readFloat(GENERAL_MESSAGE.format(operation = "multiplication"))
-    calculator.print()
+    print(calculator)
+
 
 def divide(calculator) -> None:
-        calculator.result /= readFloat(GENERAL_MESSAGE.format(operation = "division"))
-        calculator.print()
+    calculator.result /= readFloat(GENERAL_MESSAGE.format(operation = "division"))
+    print(calculator)
+
 
 def clear(calculator) -> None:
+    calculator.result = 0
     print("Cleared")
-    initialRead(calculator)
+
+
+def evaluateExpression(selection) -> float:
+    # Shunting yard algorithm
+    from calculator import Calculator
+    postfix = []
+    operators = []
+    operands = []
+    buffer = ""
+    for char in selection:
+        # numbers
+        if char.isnumeric() or char == ".":
+            buffer += char
+
+        # when we ecounter a (
+        elif char == "(":
+            if buffer != "":
+                postfix.append(float(buffer))
+                buffer = ""
+            operators.append(char)
+        
+        # when we encounter a )
+        elif char == ")":
+            if buffer != "":
+                postfix.append(float(buffer))
+                buffer = ""
+            item = operators.pop()
+            while(item != "("):
+                postfix.append(item)
+                item = operators.pop()
+            item = ""
+        
+        # when we encounter an operator
+        elif char in Calculator.OPERATORS:
+            if buffer != "":
+                postfix.append(float(buffer))
+                buffer = ""
+            if not operators:
+                operators.append(char)
+            else:
+                while operators and operators[-1] != "(" and Calculator.OPERATORS.get(char) <= Calculator.OPERATORS.get(operators[-1]):
+                    postfix.append(operators.pop())
+                operators.append(char)
+    
+    if buffer != "":
+        postfix.append(float(buffer))
+        buffer = ""
+    
+    while operators:
+        postfix.append(operators.pop())
+
+    print(postfix)
+
+    for item in postfix:
+        if isinstance(item, float):
+            operands.append(item)
+        else:
+            a = operands.pop()
+            b = operands.pop()
+            match item:
+                case "+": aux = a + b
+                case "-": aux = b - a
+                case "*": aux = a * b
+                case "/": aux = b / a
+            operands.append(aux)
+
+    return operands[0]
